a5c8e2e0551ca47f8b24de62ac6bbc50206e9b63
bvelez <Bienvenido.Velez@upr.edu>
2026-02-02 18:40:05 -0400

    Added more examples

---
+++ Variables and Expressions/fahrenheit_to_celsius.py
@@ -0,0 +1,3 @@
+fahrenheit = float(input("Please enter a temperature in F: "))
+celsius = (fahrenheit - 32) * (5 / 9)
+print(f"{fahrenheit} degrees F is equivalent to {celsius} C")
---
+++ Variables and Expressions/rectangle_perimeter.py
@@ -0,0 +1,6 @@
+width = float(input("Enter rectangle width: "))
+height = float(input("Enter rectangle height: "))
+perimeter = width + width + height + height
+print(f"The perimeter of rectangles with sides {width} and {height} is "
+      f"{perimeter}")
+
